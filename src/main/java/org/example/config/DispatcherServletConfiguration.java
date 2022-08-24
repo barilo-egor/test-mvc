12ec387c5e364876919d92d9f0279af635ae1adf
barilo-egor <EgorBarilo37552109>
2022-08-24 21:44:30 +0300

    config

---
+++ src/main/java/org/example/config/DispatcherServletConfiguration.java
@@ -0,0 +1,24 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("org.example")
+@EnableWebMvc
+public class DispatcherServletConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
+
+}
