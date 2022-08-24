47dc03fe5793de5537c79de5dadc7ad67ddbf3ec
EgorKhodikov <Khodikove18@mail.ru>
2022-08-24 22:23:48 +0300

    Хз че.

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -3,7 +3,8 @@ package org.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/main")
+@Controller
+@RequestMapping("/main")
 public class MainController {
 
     @RequestMapping("/hello")
